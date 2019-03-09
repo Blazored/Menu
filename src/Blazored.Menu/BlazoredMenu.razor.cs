512ee5c44e7a471b1e9bed2060e8f35ad8d7e650
Chris Sainty <chrissainty@users.noreply.github.com>
2019-03-09 15:14:00 +0000

    initial commit

---
+++ src/Blazored.Menu/BlazoredMenu.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazored.Menu
+{
+    public class BlazoredMenuBase : ComponentBase
+    {
+        [Parameter] protected RenderFragment ChildContent { get; set; }
+    }
+}
---
+++ src/Blazored.Menu/SubMenu.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazored.Menu
+{
+    public class SubMenuBase : ComponentBase
+    {
+        [Parameter] protected string Header { get; set; }
+        [Parameter] protected string IconClass { get; set; }
+        [Parameter] protected RenderFragment ChildContent { get; set; }
+
+        protected string Css { get; set; } = "hidden";
+        protected string Icon { get; set; } = "+";
+
+        protected void ToggleSubMenu()
+        {
+            Css = Css == "hidden" ? "visible" : "hidden";
+        }
+    }
+}

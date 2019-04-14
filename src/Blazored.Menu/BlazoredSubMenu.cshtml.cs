2deeb7488623616682e38d50d2b40d21a1fb975c
Chris Sainty <chrissainty@users.noreply.github.com>
2019-04-14 13:40:47 +0100

    Swapped to cshtml extension

---
+++ src/Blazored.Menu/BlazoredSubMenu.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazored.Menu
+{
+    public class BlazoredSubMenuBase : ComponentBase
+    {
+        [Parameter] protected string Header { get; set; }
+        [Parameter] protected string IconClass { get; set; }
+        [Parameter] protected RenderFragment ChildContent { get; set; }
+
+        protected string Icon { get; set; } = "+";
+        protected bool IsOpen { get; set; }
+
+        protected void ToggleSubMenu()
+        {
+            IsOpen = !IsOpen;
+            Icon = IsOpen ? "-" : "+";
+        }
+    }
+}
